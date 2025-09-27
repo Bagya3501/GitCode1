a6d22d51b2894e35e00bf300097479e178434a75
Bagyalakshmi <bagyalakshmi996@gmail.com>
2025-09-27 13:31:54 +0530

    upadeted alert handling

--- src/test/java/ui/Alerthandling.java
+++ src/test/java/ui/Alerthandling.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Alerthandling {
-// new git change
+// new git change to updated
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		ChromeDriver driver = new ChromeDriver();
